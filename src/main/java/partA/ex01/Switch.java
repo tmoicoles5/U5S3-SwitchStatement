34723dd293f8c7d0c66ef146b0cef6e55323c62b
tmoicoles5 <colestmoi@gmail.com>
2025-01-21 19:04:54 -0500

    switch push

--- src/main/java/partA/ex01/Switch.java
+++ src/main/java/partA/ex01/Switch.java
@@ -1,13 +1,59 @@
 package partA.ex01;
 
+import java.util.Scanner;
+
 public class Switch {
 
     public static String switchStatement(){
         String response = "";
-
         /* Put your code in between these comments : Top */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter month number 1-12:");
+        int monthNumber = scanner.nextInt();
+        String monthName;
+        switch (monthNumber){
+            case 1:
+                System.out.println("January");;
+                break;
+            case 2:
+                System.out.println(" February");
+                break;
+            case 3:
+                System.out.println("March");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            case 5:
+                System.out.println( "May");
+                break;
+            case 6:
+                System.out.println("June");
+                break;
+            case 7:
+                System.out.println("July");
+                break;
+            case 8:
+                System.out.println("August");
+                break;
+            case 9:
+                System.out.println("September");;
+                break;
+            case 10:
+                System.out.println("October");
+                break;
+            case 11 :
+                System.out.println("November");
+                break;
+            case 12 :
+                System.out.println("December");
+                break;
+            default:
+                System.out.println("Invalid month");;
+                break;
 
-
+        }
+        scanner.close();
         /* Put your code in between these comments : Bottom */
 
 
@@ -17,5 +63,6 @@ public class Switch {
     public static void main(String args[]) {
         String switchOutput = switchStatement();
         System.out.println(switchOutput);
+
     }
 }
